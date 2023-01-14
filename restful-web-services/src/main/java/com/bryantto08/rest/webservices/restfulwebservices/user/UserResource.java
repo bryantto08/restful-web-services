773ddb1495394c416afde10d50a4b80f4fa2fd60
bryantto08 <bryantto08@gmail.com>
2023-01-13 23:55:33 -0500

    HATEOAS, links to perform subsequent actions

--- restful-web-services/src/main/java/com/bryantto08/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/bryantto08/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,8 +1,10 @@
 package com.bryantto08.rest.webservices.restfulwebservices.user;
-
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import java.net.URI;
 import java.util.List;
 
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,12 +33,17 @@ public class UserResource {
 	
 	// GET METHOD /users/{id} retrieves a specific user
 	@GetMapping("/users/{id}")
-	public User retrieveUser(@PathVariable int id) {
+	public EntityModel<User> retrieveUser(@PathVariable int id) {
 		User user = service.findOne(id);
 		if (user==null) 
 			throw new UserNotFoundException("id:" + id);
-			
-		return service.findOne(id);
+		
+		// Creating an Entity Model that adds a link key to the JSON format of a user
+		EntityModel<User> entityModel = EntityModel.of(user); 
+		// This Link Builder connects to the link of retrieveAllUsers
+		WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		entityModel.add(link.withRel("all-users"));
+		return entityModel;
 	}
 	
 	// POST /users creates a user
